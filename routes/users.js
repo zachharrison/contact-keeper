1dc261e3e03593574c7d781cb13cf320067d69c0
Zach Harrison <zharrison@shaw.ca>
2021-03-04 10:49:16 -0800

    Hashed user password and finished register route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -11,14 +12,41 @@ router.post('/', [
   check('name', 'Please add name').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with at least 6 characters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  res.send('Passed')
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email }); 
+
+    if (user) {
+      res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      name, 
+      email,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send('User saved');
+
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
 });
 
 module.exports = router;
