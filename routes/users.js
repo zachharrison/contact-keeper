75d1ccffb3cdc451c966937bdb039cd4ae8470ee
Zach Harrison <zharrison@shaw.ca>
2021-03-03 21:11:26 -0800

    Added User model and validation using express validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User')
 
 // @ROUTE     POST api/users
 // @DESC      REGISTER A USER
 // @ACCESS    PUBLIC
-router.post('/', (req, res) => {
-  res.send('Registered a user :)');
+router.post('/', [
+  check('name', 'Please add name').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with at least 6 characters').isLength({ min: 6 })
+], (req, res) => {
+  const errors = validationResult(req);
+  
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  res.send('Passed')
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // CONNECT DATABASE 
 connectDB();
 
+// INIT MIDDLEWARE
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Hello World!' }))
 
 // ROUTES 
