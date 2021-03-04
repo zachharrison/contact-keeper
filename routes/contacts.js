7cc08942b42bb7170a213a952e6115ccc5a38a66
Zach Harrison <zharrison@shaw.ca>
2021-03-04 13:05:59 -0800

    Finished add contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -22,8 +22,33 @@ router.get('/', auth, async (req, res) => {
 // @ROUTE     POST api/contacts
 // @DESC      ADD A NEW CONTACT
 // @ACCESS    PRIVATE
-router.post('/', (req, res) => {
-  res.send('Added a new contact');
+router.post('/', [auth, [
+  check('name', 'Name is required').not().isEmpty()
+]] , async (req, res) => {
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, phone, type } = req.body;
+
+  try {
+    const newContact = new Contact({
+      name, 
+      email,
+      phone,
+      type,
+      user: req.user.id
+    });
+
+    const contact = await newContact.save();
+    
+    res.json(contact);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @ROUTE     PUT api/contacts/:id
