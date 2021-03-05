36ba4b81002413fcf01f986a4910dd1e5e845250
Zach Harrison <zharrison@shaw.ca>
2021-03-04 16:01:22 -0800

    Imported React router and created layout components for Navbar, Home, and About

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">About This App</h1>
+      <p className="my-1 text-center">This is a full stack React application for keeping track of your contacts</p>
+      <p className="bg-dark p text-center"><strong>Version: </strong> 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
