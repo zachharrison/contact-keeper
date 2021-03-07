30506d1a65163914b0abe84b41118c4b8f9f6552
Zach Harrison <zharrison@shaw.ca>
2021-03-07 13:14:10 -0800

    Edit server.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-const colors = require('colors');
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
@@ -7,12 +6,14 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true
     });
-    console.log('MongoDB Connected...'.cyan)
+
+    console.log('MongoDB Connected...');
   } catch (err) {
-    console.error(err.message.red);
+    console.error(err.message);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // CONNECT DATABASE 
 connectDB();
@@ -20,8 +19,12 @@ app.use('/api/contacts', require('./routes/contacts'));
 if (process.env.NODE_ENV === 'production') {
   // SET STATIC FOLDER
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`.bold.italic.red));
