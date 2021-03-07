194f7e412965c68f4b95dbce5af605936e541174
Zach Harrison <zharrison@shaw.ca>
2021-03-06 16:27:49 -0800

    Prep for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const colors = require('colors');
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,11 +11,17 @@ connectDB();
 // INIT MIDDLEWARE
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Hello World!' }))
-
 // ROUTES 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// SERVER STATIC ASSETS IN PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+  // SET STATIC FOLDER
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`.bold.italic.red));
