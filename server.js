f20914e68cd90fd3d8063a7317b4cd2224ebbb35
Zach Harrison <zharrison@shaw.ca>
2021-03-07 14:05:32 -0800

    Changes made to server

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ const app = express();
 connectDB();
 
 // INIT MIDDLEWARE
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 // ROUTES 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-
 // SET STATIC FOLDER
-app.use(express.static('build'));
-
-app.get('/*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+ 
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 
 const PORT = process.env.PORT || 5000;
